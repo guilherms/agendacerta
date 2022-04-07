3c7bece3064a72a3bcebd6b309161242ad4dfe78
guilherms1 <101368184+guilherms@users.noreply.github.com>
2022-04-07 23:23:15 +0000

    diogao

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ from flask import Flask, render_template, request, redirect
 app = Flask('app')
 
 contacts = [
-  {'name': 'João da Silva'},
-  {'name': 'Maria Souza'},
+  {'name': 'João da Silva','email':'joão@gmail.com','phone':'(16)99922-1122'},
+  {'name': 'Maria Souza', 'email':'maria1@gmail.com','phone':'(16) 99922-3333'},
 ]
 
 @app.route('/')
@@ -14,7 +14,28 @@ def index():
 @app.route('/create', methods=["POST"])
 def create():
   name = request.form.get('name')
-  contacts.append({'name': name})
+  email = request.form.get('email')
+  phone = request.form.get('phone')
+  contacts.append({
+    'name': name,
+    'email': email,
+    'phone': phone
+  })
+  return redirect('/')
+
+@app.route('/delete/<int:id>')
+def delete(id):
+  contacts.pop(id)
+  return redirect('/')
+
+@app.route('/update/<int:index>', methods=['POST'])
+def update (index):
+  name = request.form.get('name')
+  email = request.form.get('email')
+  phone = request.form.get('phone')
+  contacts[index]['name'] = name
+  contacts[index]['email'] = email
+  contacts[index]['phone'] = phone
   return redirect('/')
   
 if __name__ == '__main__':
